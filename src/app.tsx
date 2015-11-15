f37322bf2ebfd202e2269ff462be021f167e07c0
Lawrence Cheung <lawrence0819@gmail.com>
2015-11-15 15:40:24 +0800

    Dev env config

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,13 @@
+import jQuery = require('jquery');
 import React = require('react');
+import ReactDOM = require('react-dom');
+
+window['jQuery'] = jQuery;
+
+var semantic = require('../node_modules/semantic-ui/dist/semantic.js');
+
+require("../node_modules/semantic-ui/dist/semantic.css")
+
 var CommentBox = React.createClass({
   render: function() {
     return (
@@ -8,3 +17,11 @@ var CommentBox = React.createClass({
     );
   }
 });
+
+var content = document.createElement("div");
+document.body.appendChild(content);
+
+ReactDOM.render(
+  <CommentBox />,
+  content
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/app.tsx',
   output: {
@@ -14,6 +15,36 @@ module.exports = {
     }, {
       test: /\.tsx?$/,
       loader: 'ts-loader'
-    }]
-  }
+    }, {
+      test: /\.png$/,
+      loader: "url-loader?mimetype=image/png"
+    }, {
+      test: /\.jpg$/,
+      loader: "url-loader?mimetype=image/jpeg"
+    }, {
+      test: /\.gif$/,
+      loader: "url-loader?mimetype=image/gif"
+    }, {
+      test: /\.svg$/,
+      loader: "url-loader?mimetype=image/svg+xml"
+    }, {
+      test: /\.woff$/,
+      loader: "url-loader?mimetype=font/woff"
+    }, {
+      test: /\.woff2$/,
+      loader: "url-loader?mimetype=font/woff2"
+    }, {
+      test: /\.eot$/,
+      loader: "url-loader?mimetype=font/eot"
+    }, {
+      test: /\.ttf$/,
+      loader: "url-loader?mimetype=font/ttf"
+    }, ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Docker Machine Manager',
+      filename: 'index.html'
+    })
+  ]
 };
