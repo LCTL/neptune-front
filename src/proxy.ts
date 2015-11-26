444af40242ea332155433fdc93e79aba53ba1e83
Lawrence Cheung <lawrence0819@gmail.com>
2015-11-26 12:03:00 +0800

    Fix proxy bug

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,33 +2,39 @@ import * as http from 'http';
 import * as fs from 'fs';
 const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({
-  target: {
-    host: '127.0.0.1',
-    port: '3000'
-  }
+  target: 'http://127.0.0.1:3000'
 });
-const apiPath = /\/api\/(.+)/
-
+const apiPath = /\/api\/(.+)/;
+const serveAssetsFile = (req, res) => {
+  let path = __dirname + req.url;
+  fs.stat(path, (err, stat) => {
+    if (stat) {
+      if (stat.isDirectory()){
+        req.url += 'index.html';
+        serveAssetsFile(req, res);
+        return;
+      }
+      res.writeHead(200);
+      fs.createReadStream(path).pipe(res);
+    } else {
+      res.writeHead(404);
+      res.end();
+    }
+  });
+};
 http.createServer((req, res) => {
   if (apiPath.test(req.url)) {
     req.url = req.url.replace(apiPath, '/$1');
-    proxy.web(req, res);
-  } else {
-    let path = __dirname + req.url;
-    fs.stat(path, (err, stats) => {
-      if (stats.isDirectory()){
-        path += 'index.html';
-      }
-
-      fs.exists(path, (exists) => {
-        if (exists) {
-          res.writeHead(200);
-          fs.createReadStream(path).pipe(res);
-        } else {
-          res.writeHead(404);
-          res.end();
+    proxy.web(req, res, (err) => {
+      res.writeHead(500);
+      res.end(JSON.stringify({
+        error: {
+          status: 500,
+          message: err.code
         }
-      });
-    })
+      }));
+    });
+  } else {
+    serveAssetsFile(req, res);
   }
 }).listen(8080);
